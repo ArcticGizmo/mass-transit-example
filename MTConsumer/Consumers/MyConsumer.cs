fcd222073cda89a5863378ceac2d03b460dae494
Jonathan Howell <jon.howell314@gmail.com>
2022-07-29 16:53:08 +0800

    my consumer logs sent time

--- MTConsumer/Consumers/MyConsumer.cs
+++ MTConsumer/Consumers/MyConsumer.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using MassTransit;
 using MTCore.Contracts;
 using Serilog;
@@ -6,9 +7,21 @@ namespace MTConsumer.Consumers;
 
 public class MyConsumer : IConsumer<Message>
 {
+    private DateTime _epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
     public Task Consume(ConsumeContext<Message> ctx)
     {
-        Log.Information($"[MyConsumer] {ctx.Message.Name}");
+        Log.Information($"[MyConsumer] {ctx.Message.Name, 5} -- Send @ {DateTimeToMS(ctx.SentTime)}");
+
         return Task.CompletedTask;
+    }
+
+    private double DateTimeToMS(DateTime? dt)
+    {
+        if (dt == null)
+        {
+            return 0f;
+        }
+        return ((DateTime)dt).ToUniversalTime().Subtract(_epochStart).TotalMilliseconds;
     }
 }
